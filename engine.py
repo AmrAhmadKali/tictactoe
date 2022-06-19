c2fed2b47b0ef65da84832cccd9a286786b75a7c
AmrAhmadKali <69507462+AmrAhmadKali@users.noreply.github.com>
2022-06-19 20:02:40 +0200

    Add files via upload

---
+++ engine.py
@@ -0,0 +1,59 @@
+DIMENSIONS = 3
+def new_board():
+    return [[None, None, None], [None, None, None], [None, None, None]]
+
+def render(board):
+    print('\t  |0  1  2')
+    print('\t   _______')
+    for i in range(DIMENSIONS):
+        print('\t', i, end='|')
+        for j in range(DIMENSIONS):
+            if board[i][j] is not None:
+                print(board[i][j], end='  ')
+            else:
+                print(' ', end='  ')
+
+        print('')
+
+def get_move():
+    x_coord = int(input('X- coordinates : '))
+    y_coord = int(input('Y- coordinates : '))
+
+    return (x_coord, y_coord)
+
+def make_move(board, coordintes, player):
+    new_board = board
+    new_board[coordintes[0]][coordintes[1]] = player
+    return new_board
+
+def is_valid_move(board, coordinates):
+    if board[coordinates[0]][coordinates[1]] is None:
+        return True
+    return False
+
+def player(board):
+    count_x = sum([i.count('x') for i in board])
+    count_o = sum([i.count('o') for i in board])
+
+    if count_x - count_o == 1:
+        return 'o'
+
+    elif count_o - count_x == 1:
+        return 'x'
+
+    else:
+        return 'x'
+
+
+
+a = make_move([[None, 'X', None],
+         [None, 'O', None],
+         ['O', 'X', None]], (0, 0), 'X')
+
+#print(render(a))
+
+#print(is_valid_move([[None, 'X', None],[None, 'O', None],['O', 'X', None]], (2, 0)))
+
+print(player([[None, 'x', None],
+         ['x', None, None],
+         ['o', 'x', 'o']]))
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
+import numpy as np
+state = [['blank', 'X', 'blank'],
+         ['blank', 'O', 'X'],
+         ['O', 'X', 'blank']] #initial board
 
-state = [[None, None, None],
-         [None, None, None],
-         [None, None, None]] #initial board
+def ai_player(s):
+    count_X = sum([i.count('X') for i in s])
+    count_O = sum([i.count('O') for i in s])
 
-def player(s):
-    count = 1
-    if count % 2 == 0:
-        count = count + 1
+    if count_X - count_O == 1:
         return 'O'
-    else:
-        count = count + 1
+
+    elif count_O - count_X == 1:
         return 'X'
 
-def action(s, player: str):
-    moves = set()
+    else:
+        return 'X'
 
-    for i in s:             #rows
-        for j in i:         #columns
-            if j is None:
-                moves.add((i,j))
+def actions(s):
+    p = set(zip(*np.argwhere(s == 'blank')))
+    return p
 
-    return moves
+#print(player(state))
+print(actions(state))
